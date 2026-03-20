c6e457b6cb891d2428ed6477ab76a8aa297fa5a2
Deepti <sahoodiptimayee925@gmail.com>
2026-03-20 11:02:10 +0530

    new changes

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("Hii Good morning.🙌");
+  return res.send("Heyy Buguu.What's UP!!.🙌");
 });
 
 
